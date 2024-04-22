326f5d2c61baec15f48eadd9e1c9942228194626
HARSH KUMAR <harshkumarsingh54321@gmail.com>
2024-04-22 20:33:34 +0530

    deployment part

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require("cors");
+const path = require('path')
 
 require('dotenv').config(); // Load environment variables from .env file
 app.use(express.json());
 app.use(cors());
+
+// const __dirname = path.resolve()
+
 
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 const apiUrl = process.env.NEWS_API_URL;
@@ -41,6 +45,14 @@ app.post('/search', async (req, res) => {
     res.status(500).json({ message: 'Error searching news' });
   }
 });
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+
 
 // Start the server
 app.listen(port, () => {
